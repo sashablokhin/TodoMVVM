3ac34440df87dcbac78af34116aa9b74fb5c98b1
Develop <develop@develops-mac.local>
2018-04-05 05:23:38 -0700

    Initial Commit

---
+++ TodoMVVM/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TodoMVVM
+//
+//  Created by Develop on 4/5/18.
+//  Copyright © 2018 Develop. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
