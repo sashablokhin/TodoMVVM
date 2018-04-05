f176327ebebf8137f04e2d62b029aa3e2f0a1c22
Develop <develop@develops-mac.local>
2018-04-05 08:09:57 -0700

    Набросал интерфейс

--- TodoMVVM/ViewController.swift
+++ TodoMVVM/ViewController.swift
@@ -9,7 +9,9 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +22,7 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func addButtonClicked(_ sender: UIButton) {
+    }
 }
 
